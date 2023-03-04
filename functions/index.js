3fdd129a12bc73802a8187d73419b0695fc76ef3
Faozi Bouybaouene Gadrouz <fbg171@gmail.com>
2023-03-04 02:08:41 +0100

    added depndencies and checked that everything worked, but in the simulator

--- functions/index.js
+++ functions/index.js
@@ -18,7 +18,7 @@ exports.addMessage = functions.https.onRequest(async (req, res) => {
     // Grab the text parameter.
     const original = req.query.text;
     // Push the new message into Firestore using the Firebase Admin SDK.
-    const writeResult = await admin.firestore().collection('message').add({original: original});
+    const writeResult = await admin.firestore().collection('messages').add({original: original});
     // Send back a message that we've successfully written the message
     res.json({result: `Message with ID: ${writeResult.id} added.`});
   });
